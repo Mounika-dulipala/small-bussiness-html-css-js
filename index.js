a1d70ed448eae820e9612440318f1c60ffaefa00
Mounika-D <75315762+Mounika-dulipala@users.noreply.github.com>
2023-01-29 20:40:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+/*const mobileNav=document.querySelector(".mobile-navigation ul");
+document.querySelector(".mobile-navigation i").addEventListener("click",() =>{
+    console.log("clicked");
+    mobileNav.classList.contains("show") ? mobileNav.classList.remove("show"):mobileNav.classList.add("show")
+    console.log(mobileNav)
+});*/
+
+function myfun(){
+    alert("We received your data,Thank You.");
+}
